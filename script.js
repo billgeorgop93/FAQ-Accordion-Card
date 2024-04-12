efe70e9bb76351140fa07395de20fea926715b5a
Bill <bill.georgop93@gmail.com>
2024-04-12 12:47:15 +0300

    Updated with cleaner code.

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-document.querySelectorAll('.accordion-question').forEach((item) => { //Select every question to do
-    item.addEventListener('click', (event) => {
-      console.log('click!');
-      let accCollapse = item.nextElementSibling;
+document.querySelectorAll(".accordion-question").forEach(item => {
+  item.addEventListener("click", event => {
+    let accCollapse = item.nextElementSibling
 
-      if (!item.classList.contains('collapsing')) {
-        // Open item at the accordion
-        if (!item.classList.contains('open')) {
-          console.log('toggle accordion button');
+    if (!item.classList.contains("collapsing")) {
+      // Open accordion item
+      if (!item.classList.contains("open")) {
+        accCollapse.style.display = "block"
+        let accHeight = accCollapse.clientHeight
 
-          accCollapse.style.display = 'block';
-          let accHeight = accCollapse.clientHeight ;
-          console.log (accHeight);
+        setTimeout(() => {
+          accCollapse.style.height = accHeight + "px"
+          accCollapse.style.display = ""
+        }, 1)
 
-          setTimeout(() => {
-            accCollapse.style.height = accHeight + 'px';
-            accCollapse.style.display = '';
-          }, 1);
+        accCollapse.classList = "accordion-collapse collapsing"
 
-          accCollapse.classList = 'accordion-collapse collapsing';
-
-          setTimeout(() => {
-            console.log('open accordion answer');
-            accCollapse.classList = 'accordion-collapse collapse open';
-          }, 300);
-        }
-        // Close accordion item
-        else {
-            // Remove the 'collapse open' from .accordion-collapse, add 'collapsing'
-            accCollapse.classList = 'accordion-collapse collapsing';
-
-            setTimeout(() =>{
-              accCollapse.style.height = '0px';
-            }, 1);
-
-            // After x amount of time, remove 'collapsing' class and add 'collapse' class
-            setTimeout(() => {
-              console.log('close accordion answer');
-              accCollapse.classList = 'accordion-collapse collapse';
-              accCollapse.style.height = '';
-            }, 300);
-        }
-        item.classList.toggle('open');
+        setTimeout(() => {
+          accCollapse.classList = "accordion-collapse collapse open"
+        }, 300)
       }
-    });
-});
+      // Close accordion item
+      else {
+        // Remove "collapse open" from .accordion-collapse, add "collapsing"
+        accCollapse.classList = "accordion-collapse collapsing"
+
+        setTimeout(() => {
+          accCollapse.style.height = "0px"
+        }, 1)
+
+        // After X amount of time, remove "collapsing" class and add "collapse" class
+        setTimeout(() => {
+          accCollapse.classList = "accordion-collapse collapse"
+          accCollapse.style.height = ""
+        }, 300)
+      }
+
+      item.classList.toggle("open")
+    }
+  })
+})
